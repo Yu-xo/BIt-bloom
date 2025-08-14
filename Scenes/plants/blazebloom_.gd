2cfe9d5374df673e491c8e4eb0be2b006001837a
Gale Estevam <heitor@estevam.org>
2025-08-14 11:48:53 -0300

    blazebloom visuals added

--- Scenes/plants/blazebloom_.gd
+++ Scenes/plants/blazebloom_.gd
@@ -1,1 +1,10 @@
 extends BasePlant
+
+@onready var sprite: AnimatedSprite2D = $sprite
+
+func _update_stage_visuals() -> void:
+	match Stage:
+		2:
+			sprite.play("default")
+		3:
+			sprite.play("bloom")
